4449afed16ea95d52e1c06f71d0a044fe8136699
rontian <i@ronpad.com>
2022-12-26 09:26:23 +0800

    修改Singleton继承从XObject变成EventDispatcher

--- assets/utils/Singleton.ts
+++ assets/utils/Singleton.ts
@@ -4,11 +4,11 @@
 /* @date   : 2021-08-19
 *************************************************/
 
-import { XObject } from '../core/XObject';
 import { clazz } from '../decorators/clazz';
+import { EventDispatcher } from '../event/EventDispatcher';
 
 @clazz('Singleton')
-export class Singleton extends XObject {
+export class Singleton extends EventDispatcher {
     public static Instance<T extends {}>(this: new () => T): T {
         if (!(<any>this).instance) {
             (<any>this).instance = new this();
